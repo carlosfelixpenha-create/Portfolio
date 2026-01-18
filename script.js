0ba9b52a64bb7d1d8fa00c16cf1359841f362fa9
Carlos Felix <carlosfelixpenha@gmail.com>
2026-01-18 08:49:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+const btn = document.getElementById('toggle-dark');
+
+// Verifica se já existe preferência salva
+const currentMode = localStorage.getItem('mode');
+if (currentMode === 'dark') {
+  document.body.classList.add('dark');
+  btn.textContent = 'Modo Claro';
+} else {
+  btn.textContent = 'Modo Escuro';
+}
+
+btn.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+
+  if (document.body.classList.contains('dark')) {
+    btn.textContent = 'Modo Claro';
+    localStorage.setItem('mode', 'dark');
+  } else {
+    btn.textContent = 'Modo Escuro';
+    localStorage.setItem('mode', 'light');
+  }
+});
